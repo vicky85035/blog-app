6cd944725b6e805c2a165323bb1a9c16d91465dc
dheeraj <dheerajdataclaps@gmail.com>
2025-07-11 18:04:22 +0530

    refactor: update PostSerializer to use PostUserSerializer for created_by field

--- blog/serializer.py
+++ blog/serializer.py
@@ -1,19 +1,31 @@
 from rest_framework import serializers
 from blog.models import Post
+from accounts.models import User
 
+
+class PostUserSerializer(serializers.ModelSerializer):
+    name = serializers.CharField()
+    class Meta:
+        model = User
+        fields = ["id", "name", "email", "avatar"]
 
 class PostSerializer(serializers.ModelSerializer):
-    author = serializers.CharField(source= 'user.name')
-    category = serializers.CharField(source= 'story_by')
+    # created_by = serializers.SerializerMethodField()
+    created_by = PostUserSerializer(read_only=True)
 
     class Meta:
         model = Post
         fields = [
             'id',
-            'author',
-            'category',
+            'created_by',
             'title',
             'created_at',
             'cover_image',
             'description',
         ]
+
+    # def get_created_by(self, obj):
+    #     user = User.objects.filter(id=obj.created_by.id).first()
+    #     # result = {"name": user.name, "email": user.email}
+    #     result = PostUserSerializer(user).data
+    #     return result

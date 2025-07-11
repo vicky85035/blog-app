f328f06d75fb0490bb455239a626247d4b0274a2
vicky85035 <vickykumar85025@gmail.com>
2025-07-11 10:58:57 +0530

    fix: update URL patterns for user and post endpoints for consistency

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from accounts.views import UserListCreate, LoginAPIView, SignupAPIView, UserListAPIView
 
 urlpatterns = [
-    path('user_list/',UserListCreate.as_view(), name='user-list-create'),
-    path('<int:user_id>/user_list/',UserListAPIView.as_view(), name='user-api-view' ),
+    path('user/',UserListCreate.as_view(), name='user-list-create'),
+    path('<int:user_id>/user/',UserListAPIView.as_view(), name='user-api-view' ),
     path('login/',LoginAPIView.as_view(), name='login-api-view'),
     path('signup/',SignupAPIView.as_view(), name='signup-api-view'),
 ]
--- blog/urls.py
+++ blog/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from blog.views import PostListCreate, PostListAPIView
 
 urlpatterns = [
-    path('post_list/', PostListCreate.as_view(), name='post-list-create'),
-    path('<int:user_id>/post_list/',PostListAPIView.as_view(), name='post-list-api-view'),
+    path('post/', PostListCreate.as_view(), name='post-list-create'),
+    path('<int:user_id>/post/',PostListAPIView.as_view(), name='post-list-api-view'),
 ]

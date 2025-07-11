2f45abc1e76505fc9d835ee920fb4a230e8029d7
dheeraj <dheerajdataclaps@gmail.com>
2025-07-11 17:03:05 +0530

    feat: add User and Post models with migrations; configure media file handling in settings and URLs

--- blog_apl/settings.py
+++ blog_apl/settings.py
@@ -128,6 +128,10 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+# Media files (User uploads)
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
--- blog_apl/urls.py
+++ blog_apl/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
@@ -30,3 +32,6 @@ urlpatterns = [
     path('accounts/', include('accounts.urls')),
     path('blog/',include('blog.urls')),
 ]
+
+# Serve media files during development
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
